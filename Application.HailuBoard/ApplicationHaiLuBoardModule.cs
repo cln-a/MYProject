4609e824ba31e2cad521aab51fe13b97d3cfafa7
GnomeshghC <2015837457@qq.com>
2025-07-16 13:56:04 +0800

    添加项目文件。

---
+++ Application.HailuBoard/ApplicationHaiLuBoardModule.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Application.HailuBoard
+{
+    public class ApplicationHaiLuBoardModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
