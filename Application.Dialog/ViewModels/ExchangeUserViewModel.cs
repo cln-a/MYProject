ea2c5b84cffa2aa6334cd5d72457a3d87ae1153b
GnomeshghC <2015837457@qq.com>
2025-07-21 16:50:35 +0800

    添加项目文件。

---
+++ Application.Dialog/ViewModels/ExchangeUserViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Dialog
+{
+    public class ExchangeUserViewModel
+    {
+    }
+}
---
+++ Application.Dialog/Views/ExchangeUserView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Application.Dialog
+{
+    /// <summary>
+    /// ExchangeUserView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExchangeUserView : UserControl
+    {
+        public ExchangeUserView()
+        {
+            InitializeComponent();
+        }
+    }
+}
